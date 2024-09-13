5237450084a42cbc9a7c24009d837b30c332a49c
GianmarcoArz <gianmarcoarzanese@gmail.com>
2024-09-13 15:07:35 +0200

    scroll nav-bar

---
+++ base_di_partenza/assets/js.js
@@ -0,0 +1,14 @@
+const navbar = document.querySelector(".nav-bar");
+const buttonblack = document.querySelector(".buttonblack");
+const asseY = 520;
+
+const color = function () {
+  if (window.scrollY > asseY) {
+    buttonblack.classList.add("buttongreen");
+    navbar.classList.add("nav-bar-bianca");
+  } else {
+    buttonblack.classList.remove("buttongreen");
+    navbar.classList.remove("nav-bar-bianca");
+  }
+};
+window.addEventListener("scroll", color);
